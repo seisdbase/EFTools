34ce549e9516b862f7fba264868550773548c946
seisdbase <59881796+seisdbase@users.noreply.github.com>
2023-03-17 09:26:27 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// https://www.youtube.com/watch?v=1g79ZbeEeL8 - video for EF Core Power Tools
+// Disaplays data from NNs Azure NDP
+// Display all agnet no. and his license no.
+
+using EFTools.Models;
+
+
+using var db = new DBContext();
+
+var agentList = db.Agents.OrderBy(c => c.PartyKey).ToList();
+
+foreach (var agent in agentList)
+{
+    Console.WriteLine($"{agent.AgentId} : {agent.LicenseNbr}");
+}
+
+
+
+
+
